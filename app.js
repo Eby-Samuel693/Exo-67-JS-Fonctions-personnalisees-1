25ec930960cbccdc2aed9d0da7972844d1679a67
Eby-Samuel693 <coquelet.samuel@gmail.com>
2020-12-02 12:01:09 +0100

    fzfaeeqgqe

--- app.js
+++ app.js
@@ -5,30 +5,10 @@ function text(id, phrase){
 
 text("p1", "bonjour");
 
-function text2(id, phrase){
-    let para = document.getElementById(id);
-    para.innerHTML= phrase;
-}
+text("p2", "je suis le texte 2");
 
-text2("p2", "je suis le texte 2");
+text("p3", "text 3");
 
-function number(id, phrase){
-    let para = document.getElementById(id);
-    para.innerHTML= phrase;
-}
+text("p4", "le numero 4");
 
-number("p3", "text 3");
-
-function newFonction(id, phrase){
-    let para = document.getElementById(id);
-    para.innerHTML= phrase;
-}
-
-newFonction("p4", "le numero 4");
-
-function text5(id, phrase){
-    let para = document.getElementById(id);
-    para.innerHTML= phrase;
-}
-
-text5("p5", "et voila le 5");
+text("p5", "et voila le 5");

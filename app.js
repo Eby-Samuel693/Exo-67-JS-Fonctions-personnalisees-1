f6eb5832e12082b1e804a2d72d3128eaa5c85b2e
Eby-Samuel693 <coquelet.samuel@gmail.com>
2020-12-02 11:57:18 +0100

    fzfae

---
+++ app.js
@@ -0,0 +1,34 @@
+function text(id, phrase){
+    let para = document.getElementById(id);
+    para.innerHTML= phrase;
+}
+
+text("p1", "bonjour");
+
+function text2(id, phrase){
+    let para = document.getElementById(id);
+    para.innerHTML= phrase;
+}
+
+text2("p2", "je suis le texte 2");
+
+function number(id, phrase){
+    let para = document.getElementById(id);
+    para.innerHTML= phrase;
+}
+
+number("p3", "text 3");
+
+function newFonction(id, phrase){
+    let para = document.getElementById(id);
+    para.innerHTML= phrase;
+}
+
+newFonction("p4", "le numero 4");
+
+function text5(id, phrase){
+    let para = document.getElementById(id);
+    para.innerHTML= phrase;
+}
+
+text5("p5", "et voila le 5");

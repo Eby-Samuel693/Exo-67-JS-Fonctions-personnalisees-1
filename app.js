ac4ea975c90b66266078ac802d0862207a329330
Eby-Samuel693 <coquelet.samuel@gmail.com>
2020-12-03 12:44:54 +0100

    yr

--- app.js
+++ app.js
@@ -2,13 +2,8 @@ function text(id, phrase){
     let para = document.getElementById(id);
     para.innerHTML= phrase;
 }
-
 text("p1", "bonjour");
-
 text("p2", "je suis le texte 2");
-
 text("p3", "text 3");
-
 text("p4", "le numero 4");
-
 text("p5", "et voila le 5");
